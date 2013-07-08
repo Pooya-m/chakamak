a26d2657d38fd57e5762849d6c55a93ac7f10cd0
ibtkm <ibtkm2009@gmail.com>
2013-07-08 15:24:07 +0430

    another bug is fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,12 +9,15 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:user_name, :email,:password ) }
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email,:password ) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:user_name, :email, :password) }
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password) }
   end
 
   def clear(s)
     s.squeeze!(" ")
+    if !s.include? "\n" and !s.include? "\r"
+      s[(s.length / 2).floor] = s[(s.length / 2).floor] + "\n"
+    end
     s.gsub! "\n", "            "
     s.gsub! "\r", "            "
   end
