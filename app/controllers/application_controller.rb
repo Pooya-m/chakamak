7bd82e8e5b395387096ceee9b1b1b3f1e36d93c2
ibtkm <ibtkm2009@gmail.com>
2013-08-23 23:18:25 +0430

    edit user name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:user_name, :email, :password) }
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:user_name , :email , :password , :current_password) }
   end
 
   def clear(s)
