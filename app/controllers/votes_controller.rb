15c1d13fe8d43b9e2441993eb3d553cd1e5ea1be
ibtkm <ibtkm2009@gmail.com>
2013-08-25 01:39:41 +0430

    some little changes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,5 +1,6 @@
 class VotesController < ApplicationController
   before_filter :not_yourself, only: [:create]
+  before_action :prevent_double , only: [:create]
 
   def create
     @vote = current_user.votes.create(poem_id: params[:id])
@@ -14,7 +15,13 @@ class VotesController < ApplicationController
   private
     def not_yourself
       if current_user.id == Poem.find(params[:id]).user_id
-        redirect_to poems_path, alert: "Yoi can't vote to yourself!"
+        redirect_to poems_path, alert: "You can't vote to yourself!"
+      end
+    end
+
+    def prevent_double
+      if Vote.exists?(poem_id: params[:id] , user_id: current_user.id)
+        redirect_to poems_path , alert: "you can't vote twice!"
       end
     end
 end
