052ae9879889ed5463574298cc84dbd7267c9835
ibtkm <ibtkm2009@gmail.com>
2013-09-10 23:53:10 +0430

    change

--- app/models/poem.rb
+++ app/models/poem.rb
@@ -10,7 +10,7 @@ class Poem < ActiveRecord::Base
 
   validates_presence_of :content
   validates_presence_of :poet_name
-  validates_length_of :content, maximum: 102
+  validates_length_of :content, maximum: 110
 
   has_many :votes , dependent: :destroy
 
